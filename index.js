3a379e4910122734e3d3bcfb21867721d9086f84
bearwng10 <96107954+bearwng10@users.noreply.github.com>
2023-04-24 09:14:33 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const writeCards = (names, event) => {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+};
+
+const countDown = (number) => {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+};
+
+// Example of how to use the writeCards() function
+const names = ["John", "Mary", "Peter"];
+const event = "surprise";
+
+const messages = writeCards(names, event);
+
+console.log(messages); // ["Thank you, John, for the wonderful surprise gift!", "Thank you, Mary, for the wonderful surprise gift!", "Thank you, Peter, for the wonderful surprise gift!"]
+
+// Example of how to use the countDown() function
+countDown(10);
+
